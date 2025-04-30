564024d79cdcac9c6467725500ccc42689f01752
N3txu <einnerarina@gmail.com>
2025-04-30 14:39:25 -0500

    ajuste de paquetes de modulos

--- crypto-encryption-app/src/encryption.py
+++ crypto-encryption-app/src/encryption.py
@@ -1,7 +1,7 @@
-from .algorithms.rsa import rsa_encrypt, rsa_generate_keys
-from .algorithms.caesar import caesar_encrypt
-from .algorithms.aes import aes_encrypt
-from .algorithms.hybrid import hybrid_encrypt
+from algorithms.rsa import rsa_encrypt, rsa_generate_keys
+from algorithms.caesar import caesar_encrypt
+from algorithms.aes import aes_encrypt
+from algorithms.hybrid import hybrid_encrypt
 import secrets
             
 def encrypt_message(algorithm, message, key):
@@ -27,6 +27,7 @@ def encrypt_message(algorithm, message, key):
             # Incluir el desplazamiento en el resultado para descifrado
             return f"{shift}:{cipher_text}"
         elif alg == 'aes':
+
             # AES-GCM: key validated above
             return aes_encrypt(message, key)
         elif alg == 'rsa':
