917489dd3c7ebf2d72c3cee81149f812b7dd450c
N3txu <einnerarina@gmail.com>
2025-04-30 13:34:14 -0500

    fix the cross

--- crypto-encryption-app/src/API.py
+++ crypto-encryption-app/src/API.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import Optional
 from encryption import encrypt_message
@@ -8,6 +9,14 @@ from Key import default_keys
 import uvicorn
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class EncryptRequest(BaseModel):
     message: str
