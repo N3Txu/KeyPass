64855314a2091a94435566439462a8e55b6bd4f4
N3txu <einnerarina@gmail.com>
2025-04-30 14:44:59 -0500

    ajuste de paquetes de modulos

--- crypto-encryption-app/tests/__init__.py
+++
@@ -1,4 +0,0 @@
-# tests package for crypto-encryption-app
-import sys, os
-# Add project src directory to sys.path for imports
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
